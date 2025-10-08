f1d57c32cbb6dc04b5b5d5cfd1dc9c6c74967ccb
ghuannarvaez22-alt <ghuannarvaez22@gmail.com>
2025-10-08 12:07:26 +0800

    Create AddTaskView.js

---
+++ AddTaskView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddTaskView() {
+    return (
+        <div>
+            <h1>Add Task</h1>
+            <p>Here will be a form to add a new task...</p>
+        </div>
+    );
+}
+
+export default AddTaskView;
