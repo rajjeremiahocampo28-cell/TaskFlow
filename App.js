1d4c921e29288c1f805175d3f7d85ef7e02fb762
Waki1i <167158678+Waki1i@users.noreply.github.com>
2025-10-08 12:25:23 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TaskList from './TaskList';
 import AddTaskForm from './AddTaskForm';
 
 function App() {
- 
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = React.useState([]);
 
   const addTask = (taskDetails) => {
     const newTask = {
-      id: Date.now(), 
+      id: Date.now(),
       ...taskDetails,
     };
     setTasks((prevTasks) => [...prevTasks, newTask]);
   };
 
- 
   const deleteTask = (taskId) => {
     setTasks((prevTasks) => prevTasks.filter(task => task.id !== taskId));
   };
@@ -22,10 +20,7 @@ function App() {
   return (
     <div>
       <h1>Task Manager</h1>
-    
       <AddTaskForm addTask={addTask} />
-
-      
       <TaskList tasks={tasks} deleteTask={deleteTask} />
     </div>
   );
