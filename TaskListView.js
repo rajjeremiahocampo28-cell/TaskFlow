b89faf294fd3915c056c0befedba1e233636cf73
ghuannarvaez22-alt <ghuannarvaez22@gmail.com>
2025-10-08 12:06:15 +0800

    Create TaskListView.js

---
+++ TaskListView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TaskListView() {
+    return (
+        <div>
+            <h1>Task List View</h1>
+            <p>Here will be a list of tasks...</p>
+        </div>
+    );
+}
+
+export default TaskListView;
